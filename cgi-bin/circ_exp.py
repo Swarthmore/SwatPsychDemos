18458624fd8085fedbd1813231c89bf7ac4f7e3a
X.D.Zhai <xzhai1@swarthmore.edu>
2012-07-10 18:17:05 +0000

    parsing output for circle_exp

---
+++ cgi-bin/circ_exp.py
@@ -0,0 +1,31 @@
+#This script will take a JSON from the vision experiment and return a
+#propery formatted CSV file.  
+
+import sys
+import cgi
+import cgitb
+import json
+from scipy import stats
+import numpy as np
+
+cgitb.enable() #for troubleshooting
+form = cgi.FieldStorage()
+item = form.getvalue("exp_data")
+item2 = form.getvalue("option")
+json_data =  json.loads(item)
+option = json.loads(item2)
+
+if option == 'csv':
+	# print some headers so that we know what data is what
+	print "Content-Type:application/csv"
+	print "Content-Disposition:attachment;filename=dataResults.csv\n"
+	print 'Trial Results:'
+	print 'Trial Number',",", 'Reaction Time',",", 'Color Word Printed',",","True Color",",", 'Correct?',","
+	for i in range(length(json_data)):
+		trialresults = json_data[i]
+		print (str(trialresults["trial number"]) + ','
+		       str(trialresults["time"]) + ','
+		       str(trialresults["printed"]) + ','
+		       str(trialresults["true color"]) + ','
+		       str(trialresults["correct"]) + ','
+		      )
